29366968abca70d52c5afdb875c5efecbf69195c
L200180156 <44077905+L200180156@users.noreply.github.com>
2020-04-01 00:23:45 +0700

    Add files via upload

---
+++ L200180156_algostruk_modul2/Latihan21.py
@@ -0,0 +1,8 @@
+def ucapkanSalam():
+    print("Assalamu'alaikum")
+def kuadratkan(x):
+    return x*x
+
+buah = "Mangga"
+daftarBaju = ['batik','lorong','resmi berdasi']
+jumlahBaju = len(daftarBaju)
